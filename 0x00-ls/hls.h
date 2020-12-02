19871f7a54d237c840ae0185d5e8c3532a4a532c
gefranco <933@holbertonschool.com>
2020-12-02 23:06:10 +0000

    [add] header

---
+++ 0x00-ls/hls.h
@@ -0,0 +1,10 @@
+#ifndef HLS_H
+#define HLS_H
+#include <dirent.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <errno.h>
+int hls(int argc, char **argv);
+int prtfnms(struct dirent *read);
+int ldir(char *dir_name);
+#endif
