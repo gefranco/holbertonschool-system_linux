fed30a849e4bc118ab867e1917a99cb88cfe05a8
gefranco <933@holbertonschool.com>
2020-12-02 12:25:22 +0000

    [fix] compilation

--- 0x00-ls/hls.c
+++ 0x00-ls/hls.c
@@ -1,18 +1,22 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <sys/stat.h>
+int prtfnms(struct dirent *read);
 
 int hls(int argc, char **argv)
 {
 	struct dirent *read;
 	DIR *dir;
-
+	
+	(void) argc;
+        (void) argv;
 	dir = opendir(".");
 	while ((read = readdir(dir)) != NULL)
 	{
 		prtfnms(read);
 	}
 	printf("\n");
+	return (0);
 }
 
 int prtfnms(struct dirent *read)
@@ -21,4 +25,5 @@ int prtfnms(struct dirent *read)
 
 	lstat(read->d_name, &sb);
 	printf("%s ", read->d_name);
+	return (0);
 }
