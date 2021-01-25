096027dbd7959743b544af348f5d9813714d2ef4
gefranco <933@holbertonschool.com>
2021-01-25 22:47:27 +0000

    [fix] betty code style

--- 0x04-readelf/read_pipe.c
+++ 0x04-readelf/read_pipe.c
@@ -7,13 +7,13 @@ int main(int argc, char *argv[])
 	char *buf;
 	size_t s = 0;
 	char elf_str[300] = {0};
-	(void) argc;	
-	
+	(void) argc;
+
 	strcat(elf_str, "/usr/bin/readelf -W -h ");
-	strcat(elf_str,argv[1]);	
+	strcat(elf_str, argv[1]);
 
 	f = popen(elf_str, "r");
-	while( getline(&buf, &s, f) != -1)
+	while (getline(&buf, &s, f) != -1)
 		printf("%s", buf);
 	return (0);
 }
