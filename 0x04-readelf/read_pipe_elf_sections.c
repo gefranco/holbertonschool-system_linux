4394b6e02eac13a815659bf8123551cf25314f5e
gefranco <933@holbertonschool.com>
2021-01-26 14:49:08 +0000

    [add] displays the information contained in the ELF sections' headers

---
+++ 0x04-readelf/read_pipe_elf_sections.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char *argv[])
+{
+	FILE *f;
+	char *buf;
+	size_t s = 0;
+	char elf_str[300] = {0};
+	(void) argc;
+
+	strcat(elf_str, "/usr/bin/readelf -W -S ");
+	strcat(elf_str, argv[1]);
+
+	f = popen(elf_str, "r");
+	while (getline(&buf, &s, f) != -1)
+		printf("%s", buf);
+	return (0);
+}
