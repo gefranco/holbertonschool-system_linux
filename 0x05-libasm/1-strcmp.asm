d612124b53709b78e440b0bb0ee9099194137013
gefranco <933@holbertonschool.com>
2021-02-09 16:48:24 +0000

    [add] copycat of the function strcmp(3), in x86-64 Assembly

---
+++ 0x05-libasm/1-strcmp.asm
@@ -0,0 +1,50 @@
+BITS 64
+section .text
+	global asm_strcmp
+
+asm_strcmp:
+	
+	push rbp
+	mov rbp, rsp
+	
+	mov rax, rdi
+	mov rbx, rsi
+	
+	mov cl, [rax]
+	mov dl, [rbx]
+
+	cmp cl, dl
+	jne _ret_not
+	
+_compare:
+	cmp cl, 0
+	je _ret
+	
+	inc rax
+	inc rbx
+	mov cl, [rax]
+	mov dl, [rbx]
+	cmp cl, dl
+	je _compare
+
+_ret_not:
+	sub cl, dl
+	cmp cl, 0
+	jg _retg
+	mov rax, -1
+
+	mov rsp, rbp
+        pop rbp
+        ret
+_retg:
+	mov rax, 1
+	mov rsp, rbp
+        pop rbp
+        ret
+
+_ret:
+        mov rax, 0
+
+        mov rsp, rbp
+        pop rbp
+        ret
