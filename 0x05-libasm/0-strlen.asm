de5cd4dacb4d2a673c78e138df899ae2f46946d8
gefranco <933@holbertonschool.com>
2021-02-09 15:39:58 +0000

    [add] copycat of the function strlen(3), in x86-64 Assembly

---
+++ 0x05-libasm/0-strlen.asm
@@ -0,0 +1,28 @@
+section .text
+	global asm_strlen
+asm_strlen:
+	
+	push rbp
+	mov rbp, rsp
+	
+	mov rax, rdi
+	mov rbx, 0
+	mov cl, [rax]
+	cmp cl, 0
+	je _ret
+	call _count_chars
+	
+_count_chars:
+
+	inc rbx
+	inc rax
+	mov cl, [rax]
+	cmp cl, 0
+	jne _count_chars
+
+_ret:	
+	mov rax, rbx
+	
+	mov rsp, rbp
+	pop rbp
+	ret
