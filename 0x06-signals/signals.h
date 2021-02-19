bcfd8979bdfc0f2eb3256234376e73de6b420919
gefranco <933@holbertonschool.com>
2021-02-19 18:16:04 +0000

    [add] header

---
+++ 0x06-signals/signals.h
@@ -0,0 +1,8 @@
+#ifndef SIG
+#define SIG
+int handle_signal(void);
+void (*current_handler_signal(void))(int);
+int handle_sigaction(void);
+void (*current_handler_sigaction(void))(int);
+int trace_signal_sender(void);
+#endif
