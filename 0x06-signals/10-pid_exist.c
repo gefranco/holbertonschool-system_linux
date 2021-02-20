e213a9b103127c1d880098bc44310156232cd5f7
gefranco <933@holbertonschool.com>
2021-02-20 00:26:09 +0000

    [add]doc

--- 0x06-signals/10-pid_exist.c
+++ 0x06-signals/10-pid_exist.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
 #include <signal.h>
+/**
+ * pid_exist - Check if pid exist
+ * @pid: Process id
+ *
+ * Return: 1 if exist or -1 otherwise
+ */
 int pid_exist(pid_t pid)
 {
 	return (!kill(pid, 0));
