24e8cb3be85209a3a246e26012f799cb48ea860c
gefranco <933@holbertonschool.com>
2021-02-20 18:13:35 +0000

    [add]function that set a handler for the signal SIGINT

---
+++ 0x06-signals/2-handle_sigaction.c
@@ -0,0 +1,28 @@
+#include <signal.h>
+#include <stdio.h>
+/**
+ * handler - signal handlre
+ * @sig: SIGNAL number
+ * @si: sigaction
+ * @unused: old
+ * Return: nothing
+ */
+static void handler(int sig, siginfo_t *si, void *unused)
+{
+	(void)si;
+	(void)unused;
+	printf("Gotcha! [%d]\n", sig);
+}
+/**
+ * handle_sigaction - set the handler to SIGINT signal
+ * Return: 0 on success or -1 otherwise
+ */
+int handle_sigaction(void)
+{
+	struct sigaction sa;
+
+	sa.sa_sigaction = handler;
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+		return (-1);
+	return (0);
+}
