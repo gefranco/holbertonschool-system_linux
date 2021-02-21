8a44c4130733dddcf10b9959d5f9537b29194d71
gefranco <933@holbertonschool.com>
2021-02-21 00:53:39 +0000

    [add] a program that sets a handler for the signal SIGINT

---
+++ 0x06-signals/6-suspend.c
@@ -0,0 +1,40 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * handler - signal handlre
+ * @num: SIG number
+ * Return: nothing
+ */
+void handler(int num)
+{
+	printf("Caught %d\n", num);
+}
+
+/**
+ * handle_signal - set the handler to SIGINT signal
+ * Return: 0 on success or -1 otherwise
+ */
+int handle_signal(void)
+{
+
+	if (signal(SIGINT, handler) == SIG_ERR)
+		return (-1);
+	return (0);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(void)
+{
+
+	handle_signal();
+	pause();
+
+	printf("Signal received\n");
+	return (EXIT_SUCCESS);
+}
