7680900f4751fc5719a291d86e731b105b7f0477
gefranco <933@holbertonschool.com>
2021-02-21 01:00:09 +0000

    [add] a program that sends the signal SIGINT to a process, given its PID

---
+++ 0x06-signals/7-signal_send.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+/**
+ * main - Entry point
+ * @argc: args count
+ * @argv: arguments
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Usage: %s <pid>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+	kill(atoi(argv[1]), SIGABRT);
+	return (EXIT_SUCCESS);
+}
