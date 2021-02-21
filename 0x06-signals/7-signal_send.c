9a3c23cede15955b3d4fd906dfe5d645fe059639
gefranco <933@holbertonschool.com>
2021-02-21 01:07:34 +0000

    [fix] change signal

--- 0x06-signals/7-signal_send.c
+++ 0x06-signals/7-signal_send.c
@@ -15,6 +15,6 @@ int main(int argc, char *argv[])
 		printf("Usage: %s <pid>\n", argv[0]);
 		return (EXIT_FAILURE);
 	}
-	kill(atoi(argv[1]), SIGQUIT);
+	kill(atoi(argv[1]), SIGINT);
 	return (EXIT_SUCCESS);
 }
