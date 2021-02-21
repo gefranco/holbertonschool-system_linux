8ed1b838cb4182cf305b993b0b10a2e9c04c9a18
gefranco <933@holbertonschool.com>
2021-02-21 01:16:03 +0000

    [fix] return

--- 0x06-signals/7-signal_send.c
+++ 0x06-signals/7-signal_send.c
@@ -15,6 +15,7 @@ int main(int argc, char *argv[])
 		printf("Usage: %s <pid>\n", argv[0]);
 		return (EXIT_FAILURE);
 	}
-	kill(atoi(argv[1]), SIGINT);
+	if (!kill(atoi(argv[1]), SIGINT))
+		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
