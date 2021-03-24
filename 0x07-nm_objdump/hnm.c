d48be4bd13256d26b30e001a1e040689b8e699d7
gefranco <933@holbertonschool.com>
2021-03-24 22:47:04 +0000

    [fix] betty

--- 0x07-nm_objdump/hnm.c
+++ 0x07-nm_objdump/hnm.c
@@ -4,16 +4,16 @@
 int main(int argc, char *argv[])
 {
 	char *av[] = {"./hnm", "-p", "", NULL};
-	
+
 	(void) argc;
-	
+
 	av[2] = argv[1];
-	
-	if(execve("/usr/bin/nm", av, NULL) == -1)
+
+	if (execve("/usr/bin/nm", av, NULL) == -1)
 	{
 		perror("execve");
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
-	
+
 	return (EXIT_SUCCESS);
 }
