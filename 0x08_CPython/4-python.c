a38328699791d462e485198ede81b335c5f238bb
gefranco <933@holbertonschool.com>
2021-03-31 00:16:56 +0000

    [fix] print

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -6,16 +6,16 @@ void print_python_string(PyObject *p)
 	
 	if(!PyUnicode_Check(p))
 	{
-		printf(" [ERROR] Invalid String Object\n");
+		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
-	if (PyUnicode_IS_COMPACT(p) && !PyUnicode_IS_ASCII(p))
+	else if (PyUnicode_IS_COMPACT(p) && !PyUnicode_IS_ASCII(p))
                 printf("  type: compact unicode object\n");
-	if (PyUnicode_KIND(p) == PyUnicode_WCHAR_KIND)
+	else if (PyUnicode_KIND(p) == PyUnicode_WCHAR_KIND)
                 printf("  type: legacy string, not ready\n");
-	if (!PyUnicode_IS_COMPACT(p) && PyUnicode_KIND(p) != PyUnicode_WCHAR_KIND)
+	else if (!PyUnicode_IS_COMPACT(p) && PyUnicode_KIND(p) != PyUnicode_WCHAR_KIND)
                 printf("  type: legacy string, ready\n");
 	printf("  length: %ld\n", ((PyASCIIObject *)(p))->length);
 	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, '\0'));	
