138f6510bbbe2781e8cca953086d9b39d3fb7657
gefranco <933@holbertonschool.com>
2021-05-17 23:17:06 +0000

    [add] doc

--- 0x0A-multithreading/multithreading.h
+++ 0x0A-multithreading/multithreading.h
@@ -5,26 +5,54 @@
 #include <pthread.h>
 #include "list.h"
 pthread_mutex_t lock;
+/**
+ * struct pixel_s - RGB pixel
+ *
+ * @r: Red component
+ * @g: Green component
+ * @b: Blue component
+ */
 typedef struct pixel_s
 {
 	uint8_t r;
 	uint8_t g;
 	uint8_t b;
 } pixel_t;
-
+/**
+ * struct img_s - Image
+ *
+ * @w:      Image width
+ * @h:      Image height
+ * @pixels: Array of pixels
+ */
 typedef struct img_s
 {
 	size_t w;
 	size_t h;
 	pixel_t *pixels;
 } img_t;
-
+/**
+ * struct kernel_s - Convolution kernel
+ *
+ * @size:   Size of the matrix (both width and height)
+ * @matrix: Kernel matrix
+ */
 typedef struct kernel_s
 {
 	size_t size;
 	float **matrix;
 } kernel_t;
-
+/**
+ * struct blur_portion_s - Information needed to blur a portion of an image
+ *
+ * @img:      Source image
+ * @img_blur: Destination image
+ * @x:        X position of the portion in the image
+ * @y:        Y position of the portion in the image
+ * @w:        Width of the portion
+ * @h:        Height of the portion
+ * @kernel:   Convolution kernel to use
+ */
 typedef struct blur_portion_s
 {
 	img_t const *img;
@@ -41,6 +69,6 @@ void blur_portion(blur_portion_t const *portion);
 void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel);
 list_t *prime_factors(char const *s);
 
-void begin (void) __attribute__((constructor));
-void end (void) __attribute__((destructor));
+void begin(void) __attribute__((constructor));
+void end(void) __attribute__((destructor));
 #endif
