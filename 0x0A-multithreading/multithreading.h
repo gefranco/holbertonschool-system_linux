2c25b2e5681c2f59943cb0586e006445b1edda3f
gefranco <933@holbertonschool.com>
2021-05-17 23:51:58 +0000

    [testing]

--- 0x0A-multithreading/multithreading.h
+++ 0x0A-multithreading/multithreading.h
@@ -67,7 +67,7 @@ void *thread_entry(void *arg);
 int tprintf(char const *format, ...);
 void blur_portion(blur_portion_t const *portion);
 void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel);
-list_t *prime_factors(char const *s);
+/*list_t *prime_factors(char const *s);*/
 
 /*void begin(void) __attribute__((constructor));*/
 /*void end(void) __attribute__((destructor));*/
