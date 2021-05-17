ac2f76cff71ebeb1180d838d01bc35d78af9cfb5
gefranco <933@holbertonschool.com>
2021-05-17 23:13:17 +0000

    [add]doc

--- 0x0A-multithreading/0-thread_entry.c
+++ 0x0A-multithreading/0-thread_entry.c
@@ -1,10 +1,15 @@
 #include "multithreading.h"
 #include <stdio.h>
 #include <pthread.h>
+/**
+ * thread_entry -  entry point to a new thread
+ * @arg: holds the address of a string that must be printed
+ * Return: EXIT_SUCCESS
+ */
 void *thread_entry(void *arg)
 {
 	char *str = (char *)arg;
-	
+
 	printf("%s\n", str);
 	pthread_exit(NULL);
 }
