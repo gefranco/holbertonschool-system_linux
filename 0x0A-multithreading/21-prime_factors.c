39ed3fee74ace646e49ce48fc389592f0d731451
gefranco <933@holbertonschool.com>
2021-05-18 03:08:24 +0000

    [add] doc

--- 0x0A-multithreading/21-prime_factors.c
+++ 0x0A-multithreading/21-prime_factors.c
@@ -1,4 +1,9 @@
 #include "multithreading.h"
+/**
+ * prime_factors - factorizes a number into a list of prime factors
+ * @s: Number as string
+ * Return: List of prime factors
+ */
 list_t *prime_factors(char const *s)
 {
 	(void) s;
