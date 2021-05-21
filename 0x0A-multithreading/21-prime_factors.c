a07fa5149b3d056190d347b78daff6385dedf98e
gefranco <933@holbertonschool.com>
2021-05-21 23:07:27 +0000

    [add] a function that factorizes a number into a list of prime factors

--- 0x0A-multithreading/21-prime_factors.c
+++ 0x0A-multithreading/21-prime_factors.c
@@ -1,4 +1,6 @@
 #include "multithreading.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * prime_factors - factorizes a number into a list of prime factors
  * @s: Number as string
@@ -6,6 +8,33 @@
  */
 list_t *prime_factors(char const *s)
 {
-	(void) s;
-	return (NULL);
+	list_t *list = malloc(sizeof(list_t));
+	unsigned long num = strtoul(s, NULL, 10); 
+	unsigned long *p_num = malloc(sizeof(unsigned long));
+	unsigned long pf = 2;
+	*p_num = pf;
+
+	list_init(list);
+
+	list_add(list, (void *)p_num);	
+	
+	while (pf * pf <= num)
+	{
+		while(num % pf == 0)
+		{
+			p_num = malloc(sizeof(unsigned long));
+			*p_num = pf;
+			list_add(list, (void *)p_num);
+			num = num/pf;
+		}
+		pf += 1;
+	}
+	if (num > 1)
+	{
+		p_num = malloc(sizeof(unsigned long));
+		*p_num = num;
+		list_add(list, (void *)p_num);
+	}
+	
+	return (list);
 }
