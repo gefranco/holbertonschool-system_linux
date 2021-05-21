8bcbd127283a797f57249b2355d7d2444af97a75
gefranco <933@holbertonschool.com>
2021-05-21 16:30:17 +0000

    [testing]

--- 0x0A-multithreading/10-blur_portion.c
+++ 0x0A-multithreading/10-blur_portion.c
@@ -62,7 +62,7 @@ void gaussian_blur(const kernel_t *kernel, const img_t *img,
 	{
 		for (j = 0; j < kernel->size; j++, init_kernel += 1)
 		{
-			if (init_kernel < 0 || init_kernel > (int)(img->w * img->h))
+			if (init_kernel < 0 || init_kernel >= (int)(img->w * img->h))
 				continue;
 			w = kernel->matrix[i][j];
 			avg_g += (img->pixels[init_kernel].g * w);
