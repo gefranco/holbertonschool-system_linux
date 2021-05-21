351be1f6aea0a37b2e430fca707a21e7f2194875
gefranco <933@holbertonschool.com>
2021-05-21 23:14:18 +0000

    [fix]betty

--- 0x0A-multithreading/21-prime_factors.c
+++ 0x0A-multithreading/21-prime_factors.c
@@ -9,23 +9,23 @@
 list_t *prime_factors(char const *s)
 {
 	list_t *list = malloc(sizeof(list_t));
-	unsigned long num = strtoul(s, NULL, 10); 
+	unsigned long num = strtoul(s, NULL, 10);
 	unsigned long *p_num = malloc(sizeof(unsigned long));
 	unsigned long pf = 2;
 	*p_num = pf;
 
 	list_init(list);
 
-	list_add(list, (void *)p_num);	
-	
+	list_add(list, (void *)p_num);
+
 	while (pf * pf <= num)
 	{
-		while(num % pf == 0)
+		while (num % pf == 0)
 		{
 			p_num = malloc(sizeof(unsigned long));
 			*p_num = pf;
 			list_add(list, (void *)p_num);
-			num = num/pf;
+			num = num / pf;
 		}
 		pf += 1;
 	}
@@ -35,6 +35,6 @@ list_t *prime_factors(char const *s)
 		*p_num = num;
 		list_add(list, (void *)p_num);
 	}
-	
+
 	return (list);
 }
