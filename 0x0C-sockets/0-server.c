442a96673cc7c2da9084676d72ce0e397139c1ed
gefranco <933@holbertonschool.com>
2021-06-29 14:52:43 +0000

    [add]a program that opens an IPv4/TCP socket, and listens to traffic on port 12345

---
+++ 0x0C-sockets/0-server.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+int main()
+{
+	int server_socket;
+	struct sockaddr_in server_addr;
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(12345);
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	
+	bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+	while(1)	
+		listen(server_socket, 5);
+	
+	return 0;
+}
