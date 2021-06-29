1c6b60a84f4777d77fe9593e788ebf5d060449ea
gefranco <933@holbertonschool.com>
2021-06-29 14:56:06 +0000

    [fix] betty

--- 0x0C-sockets/0-server.c
+++ 0x0C-sockets/0-server.c
@@ -3,20 +3,27 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-int main()
+
+/**
+ * main - entry point.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
 	int server_socket;
 	struct sockaddr_in server_addr;
+
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(12345);
 	server_addr.sin_addr.s_addr = INADDR_ANY;
-	
+
 	bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
-	while(1)	
+	while (1)
 		listen(server_socket, 5);
-	
-	return 0;
+
+	return (0);
 }
